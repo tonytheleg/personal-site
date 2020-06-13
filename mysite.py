72e5f4c7910945525d1da9b6911efb47e849ad1b
tonytheleg <anatale@protonmail.com>
2020-06-13 13:57:16 -0400

    added resume endpoint and PDF file to site

--- mysite.py
+++ mysite.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file, current_app as app
 
 app = Flask(__name__)
 
 @app.route('/')
 def serve_site():
     return render_template('index.html')
+
+@app.route('/resume')
+def serve_pdf():
+    return send_file('static/files/Antony-Natale-Resume.pdf', attachment_filename='Antony-Natale-Resume.pdf')    
